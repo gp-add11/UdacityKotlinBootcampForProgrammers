f724d0c68d810205f0a8690de6347b9f8e8207f0
gp-add11 <iitkgp.girish@gmail.com>
2020-08-05 01:25:51 +0530

    Completed Quiz exercise 7 for fortune cookie and added file

---
+++ src/FunctionsQuizSeven.kt
@@ -0,0 +1,28 @@
+fun main(args: Array<String>) {
+    print("Your fortune is: ${getFortuneCookie()}")
+
+    //Extra practice part
+    var fortune:String
+    for (i in 1..10){
+        fortune = getFortuneCookie()
+        println("Your fortune is ${fortune}")
+        //if(fortune.contains("Take it easy")) break
+        if (fortune.toLowerCase().contains("take it easy")) break
+    }
+}
+
+fun getFortuneCookie() :String {
+    val fortunes = listOf<String>(
+        "You will have a great day!",
+        "Things will go well for you today.",
+        "Enjoy a wonderful day of success.",
+        "Be humble and all will turn out well.",
+        "Today is a good day for exercising restraint.",
+        "Take it easy and enjoy life!",
+        "Treasure your friends because they are your greatest fortune."
+    )
+    print("Enter your birthday: ")
+    val birthday: Int = readLine()?.toIntOrNull() ?: 0
+    val remainderIndex = (birthday)%(fortunes.count())
+    return fortunes[remainderIndex]
+}
