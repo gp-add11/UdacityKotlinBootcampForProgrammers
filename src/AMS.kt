2834689fd2136dec85883c692d230eb63293ba5c
gp-add11 <iitkgp.girish@gmail.com>
2020-08-07 01:26:35 +0530

    Completed exercise 11 on whether more fish can be added

--- src/AMS.kt
+++ src/AMS.kt
@@ -10,6 +10,12 @@ fun feedTheFish(){
     val food = fishFood(day)
     println("Fish will eat ${food} on ${day}")
     if (shouldChangeWater(day)) println("Change the water today")
+
+    // Testing fitMoreFish function using given examples:
+    print(canAddFish(10.0, listOf(3,3,3))) //f
+    print(canAddFish(8.0, listOf(2,2,2), hasDecorations = false)) //t
+    print(canAddFish(9.0, listOf(1,1,3), 3)) //f
+    print(canAddFish(10.0, listOf(), 7, true))//t
 }
 
 fun randomDay() : String{
@@ -32,4 +38,15 @@ fun fishFood(day : String) : String {
 
 fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
     return true
+}
+
+fun canAddFish (tankSize: Double // in gallons
+                , currentFish: List<Int>  // lengths of all fishes in tank
+                , fishSize: Int = 2 //Length of new fish to be added
+                , hasDecorations: Boolean = true // whether the tank has decorations
+                ) : Boolean {
+
+    val decorationsFactor: Double = if (hasDecorations) 0.8 else 1.0
+    return (  (tankSize.times(decorationsFactor)) >= (currentFish.sum()+fishSize) )
+
 }
