56014a8a40b2833885788271c5374277c07ae4c6
gp-add11 <iitkgp.girish@gmail.com>
2020-08-08 00:44:08 +0530

    Completed exercise 15.1 and 15.2 on whatShouldIDoToday compact functions

--- src/FunctionQuizThirteen.kt
+++ src/FunctionQuizThirteen.kt
@@ -1,11 +1,24 @@
 fun main(args: Array<String>) {
+    print("How is your mood today (sad/happy/any other): ")
+    val mood : String = readLine()!!
     println(whatShouldIDoToday("sad"))
     println(whatShouldIDoToday("happy"))
+    println(whatShouldIDoToday(mood))
 }
 
 fun whatShouldIDoToday(mood: String, weather: String = "sunny", temperature: Int = 24): String {
     return when {
+/*      ==== Commented as we will be using functions for Quiz Fifteen ====
         mood == "happy" && weather == "sunny" -> "go for a walk"
+        else -> "stay indoors"
+ */
+        isTooHot(temperature) -> "go swimming"
+        isHappySunny(mood,weather) -> "go for a walk"
+        isSadRainyCold(mood, weather, temperature) -> "stay in bed"
         else -> "stay indoors"
     }
 }
+
+fun isTooHot (temperature: Int ) = temperature > 35
+fun isHappySunny (mood: String, weather: String) = (mood == "happy" && weather == "sunny")
+fun isSadRainyCold (mood: String, weather: String, temperature: Int) = (mood == "sad" && weather == "rainy" && temperature == 0)
