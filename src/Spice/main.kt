6bcb4a0258b462176279743d9a51e652e73c1745
gp-add11 <iitkgp.girish@gmail.com>
2020-08-12 01:26:03 +0530

    Refactored code to check effects

--- src/Spice/main.kt
+++ src/Spice/main.kt
@@ -3,16 +3,22 @@ package Spice
 fun main (args: Array<String>) {
     val simpleSpice = SimpleSpice()
     println("${simpleSpice.name} ${simpleSpice.heat}")
-
+/*
     val curry: Spice = Spice("curry", "hot")
     val pepper: Spice = Spice("pepper", "very hot")
     val ginger: Spice = Spice("ginger", "little hot")
     val greenCurry: Spice = Spice("Green curry", "mild")
-    val listOfSpices = listOf<Spice>(curry,pepper,ginger,greenCurry)
+*/
+    val listOfSpices = listOf<Spice>(
+        Spice("curry", "hot"),
+        Spice("pepper", "very hot"),
+        Spice("ginger", "little hot"),
+        Spice("Green curry", "mild")
+    )
 
     val hotSpices = listOfSpices.filter { it.heat >5 }
     //println(hotSpices.forEach { it.name }) --- this is not working
-    //val spice = Spice("cayenne", "spicy")
+    val spice = Spice("cayenne", "spicy")
 
     makeSalt()
 }
