06d1c35fff463f32b24f736b334a2ae6c97b5b9d
gp-add11 <iitkgp.girish@gmail.com>
2020-08-13 00:57:33 +0530

    Added class TowerTank - inheritance

--- src/Aquarium/Aquarium.kt
+++ src/Aquarium/Aquarium.kt
@@ -1,7 +1,12 @@
 package Aquarium
 
-class Aquarium (var width: Int = 20, var length: Int = 40, var height: Int = 60) {
+open class Aquarium (var width: Int = 20, var length: Int = 40, var height: Int = 60) {
     var volume: Int
             get() = width * height * length
             set(value) {height = value/(width*length)}
+    open var water = volume * 0.9
+}
+
+class TowerTank() : Aquarium() {
+    override var water = volume * 0.8
 }
