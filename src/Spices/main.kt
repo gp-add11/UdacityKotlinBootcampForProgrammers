c6bd70ef678124b8577e9b8ee4be43f2458138a0
gp-add11 <iitkgp.girish@gmail.com>
2020-08-16 01:20:14 +0530

    Completed Classes.Quiz12 - Spices data classes

--- src/Spices/main.kt
+++ src/Spices/main.kt
@@ -2,10 +2,19 @@ package Spices
 
 fun main(args: Array<String>){
     delegate()
+
+    val spiceCabinet = listOf(SpiceContainer(Curry("Yellow Curry", "mild")),
+        SpiceContainer(Curry("Red Curry", "hot")),
+        SpiceContainer(Curry("Green Curry", "sweet")))
+    for(element in spiceCabinet) println(element.label)
 }
 
 fun delegate() {
     val curry = Curry(name = "curry", spiciness = "hot")
     println("Curry has has color ${curry.color}")
 
+}
+
+data class SpiceContainer (var spice: Spice){
+    val label = spice.name
 }
