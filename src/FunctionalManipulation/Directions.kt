c9a65dea5d2eeac89ef259523d8cebc84111dea9
gp-add11 <iitkgp.girish@gmail.com>
2020-08-30 01:01:45 +0530

    Completed Functional manipulation.Quiz3 - lambdas

---
+++ src/FunctionalManipulation/Directions.kt
@@ -0,0 +1,26 @@
+package FunctionalManipulation
+
+enum class Directions () {
+    NORTH, SOUTH, EAST, WEST, START, END
+}
+
+class Game(){
+    var path = mutableListOf<Directions>(Directions.START)
+    val north = {path.add(Directions.NORTH)}
+    val south = {path.add(Directions.SOUTH)}
+    val east = {path.add(Directions.EAST)}
+    val west = {path.add(Directions.WEST)}
+    val end = {path.add(Directions.END); println("Game Over: $path"); path.clear(); false}
+
+}
+
+fun main(args: Array<String>){
+    val game = Game()
+    println(game.path)
+    game.north()
+    game.south()
+    game.east()
+    game.west()
+    game.end()
+    println(game.path)
+}
